f0378bafc5b4c6117407aba1bdbbb50528908b91
28619426 <tienhung0182@gmail.comm>
2019-12-11 10:24:15 +0700

    Get Rentals from server, Improve rental detail, add Map

--- src/components/rental/rental-listing/RentalCard.js
+++ src/components/rental/rental-listing/RentalCard.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import {rentalType} from '../../../helpers';
 export default function RentalCard(props) {
     const {rental} = props;
     return (
         <div className={props.ColNum}>
-            <Link to={`/rentals/${rental.id}`} className="rental-detail-link">
+            <Link to={`/rentals/${rental._id}`} className="rental-detail-link">
                 <div className="card bwm-card">
-                    <img src="http://via.placeholder.com/350x250" alt="Rental Main"></img>
+                    <img className="image-card" src={rental.image} alt="Rental Main"></img>
                     <div className="card-block">
-                        <h6 className={`card-subtitle ${rental.category}`}>{rental.shared ? "Shared" : "Whole"} {rental.category} &#183; {rental.city} </h6>
+                        <h6 className={`card-subtitle ${rental.category}`}>{rentalType(rental.shared)} {rental.category} &#183; {rental.city} </h6>
                         <h4 className="card-title">{rental.title}</h4>
                         <p className="card-text">${rental.dailyRate} per Night &#183; Free Cancelation</p>
                     </div>
